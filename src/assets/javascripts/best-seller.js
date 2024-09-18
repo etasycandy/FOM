8a7387b5737bd4f9f9f15156c0d4b7d958a3f010
EtasyCandy <super.royal147@gmail.com>
2024-09-18 20:22:57 +0700

    done home page

---
+++ src/assets/javascripts/best-seller.js
@@ -0,0 +1,49 @@
+fetch("/src/assets/javascripts/data/home.json")
+.then((res) => {
+    return res.json()
+})
+.then(function(data) {
+    let render = ""
+
+    data.listBestSeller.forEach(item => {
+        render += `
+        <div class="item bg-white rounded-xl shadow-sm px-6 pb-4 relative">
+            <div>
+              <img
+                class="mb-4"
+                src="${item.image}"
+                alt="thuoc"
+              />
+              <h5 class="font-medium text-lg mb-1">${item.name}</h5>
+              <p class="text-sm font-normal mb-3">${item.category}</p>
+              <div class="price flex justify-center items-center gap-5">
+                <p class="sale font-semibold text-xl">${item.sale} VNĐ</p>
+                <div class="old text-gray-600">${item.price} VNĐ</div>
+              </div>
+            </div>
+            <div class="rounded-xl item-hover">
+              <div class="icons float-right mr-4 mt-4 flex flex-col gap-5">
+                <img
+                  class="cursor-pointer"
+                  src="/src/assets/images/icon-share.png"
+                  alt="share"
+                />
+                <img
+                  class="cursor-pointer"
+                  src="/src/assets/images/icon-like.png"
+                  alt="like"
+                />
+              </div>
+              <button
+                class="bg-white py-2 rounded shadow-sm uppercase font-medium"
+              >
+                Thêm vào giỏ
+              </button>
+            </div>
+        </div>
+        `
+    });
+
+    document.getElementById("list-bestseller").innerHTML = render
+   
+})
---
+++ src/assets/javascripts/slider-pharmacy.js
@@ -0,0 +1,21 @@
+fetch("/src/assets/javascripts/data/home.json")
+.then((res) => {
+    return res.json()
+})
+.then(function(data) {
+    let render = ""
+
+    data.listPharmacy.forEach(item => {
+        render += `<a href="${item.url}" data-id="${item.id}" class="item flex flex-col justify-center items-center">
+            <div class="image">
+                <img class="rounded-full" src="${item.image}" alt="image-pharmacy"/>
+            </div>
+            <p class="font-bold text-xl mt-5 h-10 mb-5">${item.name}</p>
+        </a>`
+    });
+
+    document.getElementById("item-pharmacy").innerHTML = render
+   
+})
+
+
